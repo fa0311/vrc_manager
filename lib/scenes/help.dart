e5ad61275c60b1b5ff6eeae508554d10a675b77c
fa0311 <yukinashi2742756@yahoo.co.jp>
2022-02-03 12:15:15 +0900

    add help page

---
+++ lib/scenes/help.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:vrchat_mobile_client/widgets/drawer.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class VRChatMobileHelp extends StatefulWidget {
+  const VRChatMobileHelp({Key? key}) : super(key: key);
+
+  @override
+  State<VRChatMobileHelp> createState() => _HelpPageState();
+}
+
+class _HelpPageState extends State<VRChatMobileHelp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('設定'),
+      ),
+      drawer: drawr(context),
+      body: Padding(
+        padding: const EdgeInsets.all(32.0),
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            children: [
+              ElevatedButton(
+                child: const Text('報告'),
+                onPressed: () {
+                  _launchURL() async {
+                    if (await canLaunch("https://github.com/fa0311/vrchat_mobile_client/issues/new")) {
+                      await launch("https://github.com/fa0311/vrchat_mobile_client/issues/new");
+                    }
+                  }
+
+                  _launchURL();
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
