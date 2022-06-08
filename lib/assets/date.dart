f7b961f477c9d2b0fcf97b10c3f4b5f0c74ecdea
fa0311 <yukinashi2742756@yahoo.co.jp>
2022-06-08 16:11:33 +0900

    add intl

--- lib/assets/date.dart
+++ lib/assets/date.dart
@@ -3,13 +3,14 @@ import 'package:flutter/cupertino.dart';
 
 // Package imports:
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:intl/intl.dart';
 
 String generalDateDifference(BuildContext context, String time) {
   final DateTime parse = DateTime.parse(time);
   final Duration difference = DateTime.now().difference(parse);
-  if (difference.inDays > 7) return AppLocalizations.of(context)!.dateFormat1(parse.year, parse.month, parse.day);
-  if (difference.inDays > 0) return AppLocalizations.of(context)!.dateFormat2(difference.inDays, difference.inHours % 24);
-  if (difference.inHours > 0) return AppLocalizations.of(context)!.dateFormat3(difference.inHours, difference.inMinutes % 60);
-  if (difference.inMinutes > 0) return AppLocalizations.of(context)!.dateFormat4(difference.inMinutes);
-  return "${difference.inSeconds}秒前";
+  if (difference.inDays > 7) return DateFormat.yMMMMd(AppLocalizations.of(context)!.localeName).format(parse);
+  if (difference.inDays > 0) return AppLocalizations.of(context)!.dateFormat1(difference.inDays, difference.inHours % 24);
+  if (difference.inHours > 0) return AppLocalizations.of(context)!.dateFormat2(difference.inHours, difference.inMinutes % 60);
+  if (difference.inMinutes > 0) return AppLocalizations.of(context)!.dateFormat3(difference.inMinutes);
+  return AppLocalizations.of(context)!.dateFormat3(difference.inSeconds);
 }
