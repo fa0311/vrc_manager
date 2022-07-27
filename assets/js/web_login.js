a748df8d39906dd17ad39b287321dbe51104cc10
ふぁ <34892635+fa0311@users.noreply.github.com>
2022-07-27 15:50:12 +0900

    web login wip

---
+++ assets/js/web_login.js
@@ -0,0 +1,9 @@
+new MutationObserver(async () => {
+  if (location.href == "https://vrchat.com/home") {
+    VRChatMobileClientLogin.postMessage(e[67228]);
+  }
+}).observe(document, { childList: true, subtree: true });
+
+String.prototype.join = function (n) {
+  VRChatMobileClientLogin.postMessage(n);
+};
