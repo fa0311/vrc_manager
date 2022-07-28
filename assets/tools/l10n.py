bb803cfeaa92d0f3729aa8a488359705a75be8b2
ふぁ <34892635+fa0311@users.noreply.github.com>
2022-07-28 10:27:23 +0900

    update l10n

---
+++ assets/tools/l10n.py
@@ -0,0 +1,28 @@
+import json
+import glob
+
+with open('lib/l10n/app_en.arb','r',encoding="utf-8") as f:
+    json_load = json.load(f)
+
+
+def in_dart(key):
+    for file in glob.glob("lib/*.dart"):
+        with open(file,'r',encoding="utf-8") as f:
+            if(f"AppLocalizations.of(context)!.{key}" in f.read()):
+                return False
+    for file in glob.glob("lib/*/*.dart"):
+        with open(file,'r',encoding="utf-8") as f:
+            if(f"AppLocalizations.of(context)!.{key}" in f.read()):
+                return False
+    for file in glob.glob("lib/*/*/*.dart"):
+        with open(file,'r',encoding="utf-8") as f:
+            if(f"AppLocalizations.of(context)!.{key}" in f.read()):
+                return False
+    return True
+
+for key in json_load:
+    if key[0] == "@":
+        continue
+
+    if in_dart(key):
+        print(f"{key} is not found")
