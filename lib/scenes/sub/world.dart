ccb29e732b2cfb0536d80f1581d3ae920eb5cc22
ふぁ <34892635+fa0311@users.noreply.github.com>
2022-10-27 08:11:50 +0900

    add world "#120

--- lib/scenes/sub/world.dart
+++ lib/scenes/sub/world.dart
@@ -8,7 +8,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 // Project imports:
 import 'package:vrc_manager/api/data_class.dart';
 import 'package:vrc_manager/api/main.dart';
-import 'package:vrc_manager/assets/error.dart';
 import 'package:vrc_manager/assets/flutter/text_stream.dart';
 import 'package:vrc_manager/main.dart';
 import 'package:vrc_manager/widgets/drawer.dart';
@@ -16,47 +15,66 @@ import 'package:vrc_manager/widgets/modal/list_tile/main.dart';
 import 'package:vrc_manager/widgets/modal/list_tile/world.dart';
 import 'package:vrc_manager/widgets/world.dart';
 
-class VRChatMobileWorld extends ConsumerWidget {
-  VRChatMobileWorld({Key? key, required this.worldId}) : super(key: key);
-  late final String worldId;
-  late final VRChatAPI vrchatLoginSession = VRChatAPI(cookie: appConfig.loggedAccount?.cookie ?? "");
-  late final VRChatWorld? world;
+class VRChatMobileWorldData {
+  VRChatWorld world;
 
-  Future get(BuildContext context) async {
-    world = await vrchatLoginSession.worlds(worldId).catchError((status) {
-      apiError(context, status);
-    });
-  }
+  VRChatMobileWorldData({
+    required this.world,
+  });
+}
+
+final vrchatMobileUserProvider = FutureProvider.family<VRChatMobileWorldData, String>((ref, worldId) async {
+  final VRChatAPI vrchatLoginSession = VRChatAPI(cookie: appConfig.loggedAccount?.cookie ?? "");
+  late VRChatWorld world;
+  await Future.wait([vrchatLoginSession.worlds(worldId).then((value) => world = value)]);
+  return VRChatMobileWorldData(world: world);
+});
+
+class VRChatMobileWorld extends ConsumerWidget {
+  const VRChatMobileWorld({Key? key, required this.worldId}) : super(key: key);
+  final String worldId;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     textStream(context);
+    AsyncValue<VRChatMobileWorldData> data = ref.watch(vrchatMobileUserProvider(worldId));
+
     return Scaffold(
-      appBar: AppBar(title: Text(AppLocalizations.of(context)!.world), actions: [
-        if (world != null)
-          IconButton(
-            icon: const Icon(Icons.more_vert),
-            onPressed: () => modalBottom(
-              context,
-              worldDetailsModalBottom(context, ref, world!),
-            ),
-          )
-      ]),
+      appBar: AppBar(
+        title: Text(AppLocalizations.of(context)!.world),
+        actions: data.when(
+          loading: () => null,
+          error: (err, stack) => null,
+          data: (data) => [
+            IconButton(
+              icon: const Icon(Icons.more_vert),
+              onPressed: () => modalBottom(
+                context,
+                worldDetailsModalBottom(context, ref, data.world),
+              ),
+            )
+          ],
+        ),
+      ),
       drawer: Navigator.of(context).canPop() ? null : drawer(),
       body: SafeArea(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          child: SingleChildScrollView(
-            child: Container(
-              padding: const EdgeInsets.only(top: 10, bottom: 0, right: 30, left: 30),
-              child: Column(
-                children: [
-                  if (world == null) const Padding(padding: EdgeInsets.only(top: 30), child: CircularProgressIndicator()),
-                  if (world != null) ...[
-                    worldProfile(context, world!),
-                  ]
-                ],
-              ),
+        child: SingleChildScrollView(
+          child: Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.only(
+              top: 10,
+              bottom: 0,
+              right: 30,
+              left: 30,
+            ),
+            child: Consumer(
+              builder: (context, ref, child) {
+                return data.when(
+                  loading: () => const Center(child: CircularProgressIndicator()),
+                  error: (err, stack) => Text('Error: $err'),
+                  data: (data) => worldProfile(context, data.world),
+                );
+              },
             ),
           ),
         ),
