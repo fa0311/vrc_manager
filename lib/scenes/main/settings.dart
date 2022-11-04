931e79a45629840f4ad20f6c8b26bf336e8f2c20
ふぁ <34892635+fa0311@users.noreply.github.com>
2022-11-04 09:41:15 +0900

    add #120 VRChatMobileSettings

--- lib/scenes/main/settings.dart
+++ lib/scenes/main/settings.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 // Package imports:
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 // Project imports:
 import 'package:vrc_manager/assets/flutter/text_stream.dart';
@@ -13,16 +14,11 @@ import 'package:vrc_manager/scenes/setting/help.dart';
 import 'package:vrc_manager/scenes/setting/permissions.dart';
 import 'package:vrc_manager/widgets/drawer.dart';
 
-class VRChatMobileSettings extends StatefulWidget {
+class VRChatMobileSettings extends ConsumerWidget {
   const VRChatMobileSettings({Key? key}) : super(key: key);
 
   @override
-  State<VRChatMobileSettings> createState() => _SettingPageState();
-}
-
-class _SettingPageState extends State<VRChatMobileSettings> {
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     textStream(context);
     return Scaffold(
       appBar: AppBar(
@@ -30,85 +26,80 @@ class _SettingPageState extends State<VRChatMobileSettings> {
       ),
       drawer: appConfig.isLogout() ? drawer() : simpleDrawer(),
       body: SafeArea(
-        child: SizedBox(
-          child: SingleChildScrollView(
-            child: Container(
-              padding: const EdgeInsets.only(top: 10),
-              child: Column(
-                children: <Widget>[
-                  ListTile(
-                    leading: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const <Widget>[
-                        Icon(
-                          Icons.accessibility,
-                        ),
-                      ],
-                    ),
-                    title: Text(AppLocalizations.of(context)!.accessibility),
-                    onTap: () => Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (BuildContext context) => const VRChatMobileSettingsAccessibility(),
+        child: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.only(top: 10),
+            child: Column(
+              children: <Widget>[
+                ListTile(
+                  leading: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const <Widget>[
+                      Icon(
+                        Icons.accessibility,
                       ),
+                    ],
+                  ),
+                  title: Text(AppLocalizations.of(context)!.accessibility),
+                  onTap: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (BuildContext context) => const VRChatMobileSettingsAccessibility(),
                     ),
                   ),
-                  const Divider(),
-                  ListTile(
-                    leading: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const <Widget>[
-                        Icon(
-                          Icons.account_circle,
-                        ),
-                      ],
-                    ),
-                    title: Text(AppLocalizations.of(context)!.account),
-                    onTap: () => Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (BuildContext context) => const VRChatMobileSettingsAccount(),
+                ),
+                ListTile(
+                  leading: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const <Widget>[
+                      Icon(
+                        Icons.account_circle,
                       ),
+                    ],
+                  ),
+                  title: Text(AppLocalizations.of(context)!.account),
+                  onTap: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (BuildContext context) => const VRChatMobileSettingsAccount(),
                     ),
                   ),
-                  const Divider(),
-                  ListTile(
-                    leading: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const <Widget>[
-                        Icon(
-                          Icons.admin_panel_settings,
-                        ),
-                      ],
-                    ),
-                    title: Text(AppLocalizations.of(context)!.permissions),
-                    onTap: () => Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (BuildContext context) => const VRChatMobileSettingsPermissions(),
+                ),
+                ListTile(
+                  leading: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const <Widget>[
+                      Icon(
+                        Icons.admin_panel_settings,
                       ),
+                    ],
+                  ),
+                  title: Text(AppLocalizations.of(context)!.permissions),
+                  onTap: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (BuildContext context) => const VRChatMobileSettingsPermissions(),
                     ),
                   ),
-                  const Divider(),
-                  ListTile(
-                    leading: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const <Widget>[
-                        Icon(
-                          Icons.help,
-                        ),
-                      ],
-                    ),
-                    title: Text(AppLocalizations.of(context)!.help),
-                    onTap: () => Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (BuildContext context) => const VRChatMobileHelp(),
+                ),
+                ListTile(
+                  leading: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const <Widget>[
+                      Icon(
+                        Icons.help,
                       ),
+                    ],
+                  ),
+                  title: Text(AppLocalizations.of(context)!.help),
+                  onTap: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (BuildContext context) => const VRChatMobileHelp(),
                     ),
-                  )
-                ],
-              ),
+                  ),
+                )
+              ],
             ),
           ),
         ),
