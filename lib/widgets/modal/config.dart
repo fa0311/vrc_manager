3c94dd75de6681f0a5a07f6f8baebb28a9cf9764
ふぁ <yuki@yuki0311.com>
2022-12-10 20:36:22 +0900

    add modal congig wip
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- lib/widgets/modal/config.dart
+++ lib/widgets/modal/config.dart
@@ -1,3 +1,5 @@
+import 'package:vrc_manager/api/data_class.dart';
+
 enum UserModalType {
   self,
   onlineFriends,
@@ -8,22 +10,63 @@ enum UserModalType {
 class UserModalData {
   bool editBio = false;
   bool editNote = false;
-  bool shareUrl = false;
   bool profileAction = false;
+  Uri? shareUrl;
 }
 
 UserModalData getUserModalConfig({
   required UserModalType type,
-  required String searchingText,
+  required VRChatUser user,
 }) {
   switch (type) {
     case UserModalType.self:
-      return UserModalData();
+      return UserModalData()
+        ..editBio = true
+        ..editNote = true
+        ..shareUrl = Uri.https("vrchat.com", "/home/user/${user.id}");
     case UserModalType.onlineFriends:
-      return UserModalData();
+      return UserModalData()
+        ..editNote = true
+        ..profileAction = true
+        ..shareUrl = Uri.https("vrchat.com", "/home/user/${user.id}");
     case UserModalType.offlineFriends:
-      return UserModalData();
+      return UserModalData()
+        ..editNote = true
+        ..profileAction = true
+        ..shareUrl = Uri.https("vrchat.com", "/home/user/${user.id}");
     case UserModalType.user:
-      return UserModalData();
+      return UserModalData()
+        ..editNote = true
+        ..profileAction = true
+        ..shareUrl = Uri.https("vrchat.com", "/home/user/${user.id}");
+  }
+}
+
+enum WorldModalType {
+  instance,
+  world;
+}
+
+class WorldModalData {
+  bool selfInvite = false;
+  bool favorite = false;
+  bool shareInstance = false;
+  Uri? shareUrl;
+}
+
+WorldModalData getWorldModalConfig({
+  required WorldModalType type,
+  required VRChatUser world,
+}) {
+  switch (type) {
+    case WorldModalType.instance:
+      return WorldModalData()
+        ..selfInvite = true
+        ..favorite = true
+        ..shareInstance = true;
+    case WorldModalType.world:
+      return WorldModalData()
+        ..favorite = true
+        ..shareUrl = Uri.https("vrchat.com", "/home/world/${world.id}");
   }
 }
