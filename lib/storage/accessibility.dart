4011ba50e687e510cc795d76e3954354471dac5e
ふぁ <yuki@yuki0311.com>
2022-12-31 03:13:34 +0900

    fix error
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- lib/storage/accessibility.dart
+++ lib/storage/accessibility.dart
@@ -20,9 +20,9 @@ class AccessibilityConfigNotifier extends ChangeNotifier {
 
   AccessibilityConfigNotifier() {
     Future.wait([
-      getStorage("language_code").then((String? value) => languageCode = LanguageCode.values.byName(value!)),
-      getStorage("theme_brightness").then((String? value) => themeBrightness = ThemeBrightness.values.byName(value!)),
-      getStorage("dark_theme_brightness").then((String? value) => themeBrightness = ThemeBrightness.values.byName(value!)),
+      getStorage("language_code").then((String? value) => languageCode = LanguageCode.values.byName(value ?? languageCode.name)),
+      getStorage("theme_brightness").then((String? value) => themeBrightness = ThemeBrightness.values.byName(value ?? themeBrightness.name)),
+      getStorage("dark_theme_brightness").then((String? value) => darkThemeBrightness = ThemeBrightness.values.byName(value ?? darkThemeBrightness.name)),
       getStorage("force_external_browser").then((String? value) => forceExternalBrowser = (value == "true")),
       getStorage("debug_mode").then((String? value) => debugMode = (value == "true")),
     ]).whenComplete(() => notifyListeners());
