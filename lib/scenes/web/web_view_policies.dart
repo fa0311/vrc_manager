251a335172adcc95539296ee176e2d38f2c5f9cc
ふぁ <yuki@yuki0311.com>
2023-01-03 16:08:17 +0900

    fix color
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- lib/scenes/web/web_view_policies.dart
+++ lib/scenes/web/web_view_policies.dart
@@ -47,48 +47,47 @@ class VRChatMobileWebViewUserPolicy extends ConsumerWidget {
           ),
         ],
       ),
-      bottomNavigationBar: BottomAppBar(
-        child: Container(
-          decoration: BoxDecoration(
-            boxShadow: [
-              BoxShadow(
-                color: Theme.of(context).colorScheme.shadow.withOpacity(0.2),
-                spreadRadius: 5,
-                blurRadius: 5,
-              ),
-            ],
-          ),
-          child: SizedBox(
-            height: 50,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Spacer(),
-                Expanded(
-                  flex: 2,
-                  child: ElevatedButton(
-                    onPressed: () => SystemNavigator.pop(),
-                    style: ElevatedButton.styleFrom(backgroundColor: Colors.grey),
-                    child: Text(
-                      AppLocalizations.of(context)!.disagree,
-                    ),
-                  ),
-                ),
-                const Spacer(),
-                Expanded(
-                  flex: 2,
-                  child: ElevatedButton(
-                    onPressed: () async {
-                      ref.read(userPolicyConfigProvider).setAgree(true);
-                    },
-                    child: Text(
-                      AppLocalizations.of(context)!.agree,
-                    ),
-                  ),
-                ),
-                const Spacer(),
-              ],
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: Theme.of(context).cardColor,
+          boxShadow: [
+            BoxShadow(
+              color: Theme.of(context).colorScheme.shadow.withOpacity(0.2),
+              spreadRadius: 5,
+              blurRadius: 5,
             ),
+          ],
+        ),
+        child: SizedBox(
+          height: 50,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Spacer(),
+              Expanded(
+                flex: 2,
+                child: ElevatedButton(
+                  onPressed: () => SystemNavigator.pop(),
+                  style: ElevatedButton.styleFrom(backgroundColor: Colors.grey),
+                  child: Text(
+                    AppLocalizations.of(context)!.disagree,
+                  ),
+                ),
+              ),
+              const Spacer(),
+              Expanded(
+                flex: 2,
+                child: ElevatedButton(
+                  onPressed: () async {
+                    ref.read(userPolicyConfigProvider).setAgree(true);
+                  },
+                  child: Text(
+                    AppLocalizations.of(context)!.agree,
+                  ),
+                ),
+              ),
+              const Spacer(),
+            ],
           ),
         ),
       ),
@@ -107,29 +106,27 @@ class VRChatMobileWebViewUserPolicy extends ConsumerWidget {
             );
           });
 
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  AppLocalizations.of(context)!.lookAtYourBrowser,
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                AppLocalizations.of(context)!.lookAtYourBrowser,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(16.0),
+                child: SizedBox(
+                  width: 50,
+                  height: 50,
+                  child: CircularProgressIndicator(
+                    strokeWidth: 6,
                   ),
                 ),
-                const Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: SizedBox(
-                    width: 50,
-                    height: 50,
-                    child: CircularProgressIndicator(
-                      strokeWidth: 6,
-                    ),
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           );
         }
       }(),
