91f9623087f177518cd44e9c684eb141b72a38c7
ふぁ <yuki@yuki0311.com>
2023-01-07 09:55:30 +0900

    fix google play url
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- lib/assets.dart
+++ lib/assets.dart
@@ -10,7 +10,7 @@ class Assets {
   static Uri repository = Assets.github.resolve("/fa0311/vrc_manager");
   static Uri issues = Assets.github.resolve("/fa0311/vrc_manager/issues/new/choose");
   static Uri contact = Assets.twitter.resolve("/faa0311");
-  static Uri rate = Assets.twitter.replace(path: "/store/apps/details", queryParameters: {"id": "com.yuki0311.vrc_manager"});
+  static Uri rate = Assets.googlePlay.replace(path: "/store/apps/details", queryParameters: {"id": "com.yuki0311.vrc_manager"});
   static Uri release = Assets.github.resolve("/fa0311/vrc_manager/releases");
   static Uri report = Assets.github.replace(path: "/fa0311/vrc_manager/issues/new", queryParameters: {"template": "redirected-from-app.yml"});
   static Uri userPolicy = Assets.github.resolve("/fa0311/vrc_manager/blob/master/docs/user_policies/ja.md");
